fd9133c6e4edca4ebdead3c1b99f98a449592efa
이다은 <daeun.lee@coinplug.com>
2019-08-01 16:22:22 +0900

    login 구현중

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
-require("dotenv").config();
+import "./env";
 import { GraphQLServer } from "graphql-yoga";
+import { prisma } from "../generated/prisma-client";
 import logger from "morgan";
+import passport from "passport";
 import schema from "./schema";
+import {authenticateJwt} from "./passport";
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({ schema });
+const server = new GraphQLServer({ schema, context:{prisma} });
 
 server.express.use(logger("dev"));
+server.express.use(authenticateJwt);
 
 server.start({port: PORT}, () => console.log(`Server running on http://localhost:${PORT}`));
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,34 @@
+import "./env";
 import { adjectives, nonces } from "./word";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
+import jwt from "jsonwebtoken";
 
 export const generateSecret = () => {
     const randomNumber = Math.floor(Math.random() * adjectives.length);
     return `${adjectives[randomNumber]} ${nonces[randomNumber]}`;
 };
+
+const sendMail = (email) => {
+    const options = {
+        auth: {
+            api_user: process.env.SENDGRID_USERNAME,
+            api_key: process.env.SENDGRID_PASSWORD
+        }
+    }
+
+    const client = nodemailer.createTransport(sgTransport(options));
+    return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+    const email = {
+        from: "2da@prismagram.com",
+        to: address,
+        subject: "Login Secret for Prismagram",
+        html: `Hello! Your login secret is <strong>${secret}.</strong><br/>Copy paste on the app/website to login`
+    }
+    return sendMail(email);
+};
+
+export const generateToken = (id) => jwt.sign({id}, process.env.JWT_SECRET);

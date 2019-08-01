98ce903b70a14e0d05a795608d18b1cfc40f611a
이다은 <daeun.lee@coinplug.com>
2019-08-01 16:22:35 +0900

    로그인 구현중

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -2,7 +2,7 @@ import {prisma} from "../../../../generated/prisma-client";
 
 export default {
     Mutation: {
-        createAccount: async(_, args) => {
+        createAccount: async(_, args, {prisma}) => {
             const { username, email, bio = ""} = args;
             const user = await prisma.createUser({username, email, bio});
             return user;

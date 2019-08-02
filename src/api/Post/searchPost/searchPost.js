184cf3321a3dc9cb16e6393cec11fcd19de856ea
이다은 <daeun.lee@coinplug.com>
2019-08-02 14:35:29 +0900

    search 구현

---
+++ src/api/Post/searchPost/searchPost.js
@@ -0,0 +1,12 @@
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+    Query: {
+        searchPost: async(_, args) => prisma.posts({where: {
+            OR: [
+                {location_starts_with: args.terms}, 
+                {caption_starts_withx: args.terms}
+            ]
+        }})
+    }
+}
